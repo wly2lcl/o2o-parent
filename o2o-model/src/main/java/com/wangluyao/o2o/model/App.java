1b241dc64f481c3c6bcc79798d251b3ea6a16eca
王璐瑶 <1347888240@qq.com>
2019-07-04 10:20:35 +0800

    初始化

---
+++ o2o-model/src/main/java/com/wangluyao/o2o/model/App.java
@@ -0,0 +1,13 @@
+package com.wangluyao.o2o.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
